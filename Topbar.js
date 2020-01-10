1939fbe073904ac99c7d040c9127006f75c8f14b
Lajat <51463030+Lajat@users.noreply.github.com>
2020-01-11 00:54:56 +0530

    Delete Topbar.js

--- Topbar.js
+++
@@ -1,8 +0,0 @@
-var shoplaneHomepage = document.getElementById('shoplane-homepage');
-shoplaneHomepage.onclick = function() {
-    location.assign("Homepage.html");
-}
-var cart = document.getElementById('cart');
-cart.onclick = function() {
-    location.assign("Check Out Page.html");
-}
